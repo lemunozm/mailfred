8fe7e0eb1b19c6ed83469740ea0583932de07382
lemunozm <lemunozm@gmail.com>
2023-06-08 20:35:23 +0200

    improve default logger

--- src/logger.rs
+++ src/logger.rs
@@ -11,15 +11,23 @@ pub fn configure(level_filter: log::LevelFilter) {
 
     fern::Dispatch::new()
         .level(level_filter)
-        .filter(move |metadata| metadata.target().starts_with("mailfred"))
+        .filter(move |metadata| {
+            metadata.target().eq("mailfred")
+                || metadata.target().starts_with("mailfred::")
+                || metadata.target().eq("app")
+        })
         .format(move |out, message, record| {
             out.finish(format_args!(
-                "[{}] [{}] {}",
+                "[{}] [{}] {} => {}",
                 chrono::Local::now()
                     .format("%Y-%m-%d %H:%M:%S")
                     .to_string()
                     .white(),
                 level_colors.color(record.level()),
+                match record.target() {
+                    "app" => "app",
+                    _ => "mailfred",
+                },
                 message
             ))
         })
